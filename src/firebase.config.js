5cc9e6881e515d559068411f25e61321084eacf1
Shamanta Sristy <shamantasristy@gmail.com>
2021-03-18 22:49:59 +0600

    google,facebook and github authentication active

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAWFxzCuK2EMCRv4L4MQpVCKpVjb9xB4Bg",
+    authDomain: "fir-recap-b84e8.firebaseapp.com",
+    projectId: "fir-recap-b84e8",
+    storageBucket: "fir-recap-b84e8.appspot.com",
+    messagingSenderId: "53909621242",
+    appId: "1:53909621242:web:b9958a6e5147fb0f9cfc43"
+  };
+  export default firebaseConfig;
